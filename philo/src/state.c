6f611f2aa4b175b572593e0aa452df50d1ba8a92
LaurinUB <Laurinub@yahoo.de>
2023-02-21 15:29:23 +0100

    norm and refactor

--- philo/src/state.c
+++ philo/src/state.c
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 11:12:31 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/02/21 15:23:29 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/02/21 15:26:45 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,33 +57,4 @@ void	slp(t_philo *philo)
 		pthread_mutex_unlock(&philo->time->print);
 		sleep_ms(philo->time->time_to_sleep);
 	}
-}
-
-int	check_death(t_philo	*philo)
-{
-	if (philo->state != DEAD && time_in_ms() - philo->last_meal
-		>= (unsigned long)philo->time->time_to_die)
-	{
-		printf("%s%lu	philo %d	died	⚰️\n", get_philo_color(philo),
-			time_in_ms() - philo->time->start_time, philo->number);
-		philo->state = DEAD;
-		return (1);
-	}
-	if (philo->done)
-		return (1);
-	return (0);
-}
-
-int	check_for_life(t_philo **philos)
-{
-	int	i;
-
-	i = 0;
-	while (philos[i])
-	{
-		if (philos[i]->state == DEAD)
-			return (1);
-		i++;
-	}
-	return (0);
 }
---
+++ philo/src/thread.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   thread.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 15:27:31 by luntiet-          #+#    #+#             */
+/*   Updated: 2023/02/21 15:27:40 by luntiet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	myturn(t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	if (philo->number % 2 == 0)
+		sleep_ms(3);
+	while (philo->state != DEAD)
+	{
+		pthread_mutex_lock(&philo->time->print);
+		pthread_mutex_unlock(&philo->time->print);
+		eat(philo);
+		if (philo->time->meal_count >= 0 && i >= philo->time->meal_count)
+		{
+			philo->done = 1;
+			break ;
+		}
+		slp(philo);
+		think(philo);
+		i++;
+	}
+}
+
+void	state_check(t_check *checker)
+{
+	int	i;
+
+	i = 0;
+	while (!check_for_life(checker->philos))
+	{
+		if (!checker->philos[i])
+			i = 0;
+		pthread_mutex_lock(&checker->philos[0]->time->print);
+		if (check_death(checker->philos[i]))
+		{
+			pthread_detach(checker->philos[i]->tid);
+			pthread_mutex_destroy(&checker->philos[i]->time->print);
+			break ;
+		}
+		pthread_mutex_unlock(&checker->philos[i]->time->print);
+		i++;
+	}
+}
