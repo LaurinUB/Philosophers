45d295b868c4e44b89e8867db14a591cb52693a3
LaurinUB <Laurinub@yahoo.de>
2023-03-01 11:17:31 +0100

    done?

--- philo_bonus/src/main_bonus.c
+++ philo_bonus/src/main_bonus.c
@@ -6,11 +6,26 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 19:25:05 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/02/28 17:46:55 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/03/01 11:15:49 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
+
+void	kill_all(t_philo **philos, t_time *tv)
+{
+	int	i;
+
+	i = 0;
+	while (i < tv->number)
+	{
+		pthread_detach(philos[i]->tid);
+		kill(philos[i]->pid, SIGINT);
+		free(philos[i]);
+		i++;
+	}
+	free(philos);
+}
 
 void	check_death(t_philo *philo)
 {
@@ -18,14 +33,15 @@ void	check_death(t_philo *philo)
 	{
 		sem_wait(philo->time->death);
 		if (time_in_ms() - philo->last_meal
-			>= (unsigned long)philo->time->time_to_die)
+			> (unsigned long)philo->time->time_to_die)
 		{
 			sem_wait(philo->time->print);
-			// printf("%lu	philo %d	died	⚰️\n",
-			// 	time_in_ms() - philo->time->start_time, philo->number);
-			philo->state = DEAD;
+			printf("%lu	philo %d	died	⚰️\n",
+				time_in_ms() - philo->time->start_time, philo->number);
+			kill_all(&philo, philo->time);
 			exit(2);
 		}
+		sleep_ms(1);
 		sem_post(philo->time->death);
 	}
 }
@@ -39,44 +55,22 @@ void	creat_child(t_philo *philo)
 	if (philo->pid == 0)
 	{
 		if (philo->number % 2 == 0)
-			sleep_ms(10);
+			sleep_ms(3);
 		pthread_create(&philo->tid, NULL, (void *)check_death, philo);
 		while (philo->state != DEAD)
 		{
-			putendl_fd("state loop", 2);
 			sem_wait(philo->time->death);
 			sem_post(philo->time->death);
 			eat(philo);
 			if (philo->time->meal_count >= 0 && i == philo->time->meal_count)
-				exit (ERROR);
-			sem_wait(philo->time->death);
-			sem_post(philo->time->death);
+			{
+				return ;
+			}
 			slp(philo);
-			sem_wait(philo->time->death);
-			sem_post(philo->time->death);
 			think(philo);
 			i++;
 		}
 	}
-}
-
-void	kill_all(t_philo **philos, t_time *tv)
-{
-	int	i;
-
-	i = 0;
-	while (i < tv->number)
-	{
-		putendl_fd("kill loop", 2);
-		if (philos[i])
-		{
-			pthread_detach(philos[i]->tid);
-			kill(philos[i]->pid, SIGQUIT);
-			free(philos[i]);
-		}
-		i++;
-	}
-	// free(tv);
 }
 
 int	main(int argc, char **argv)
@@ -99,11 +93,9 @@ int	main(int argc, char **argv)
 	i = -1;
 	while (philos[++i])
 	{
-		putendl_fd("wait loop", 2);
 		waitpid(philos[i]->pid, &status, 0);
 		if (status)
-			return (kill_all(philos, tv), close_sem(tv), free(tv), SUCCESS);
+			break ;
 	}
-	close_sem(tv);
-	return (SUCCESS);
+	return (kill_all(philos, tv), close_sem(tv), free(tv), SUCCESS);
 }
--- philo_bonus/src/state_bonus.c
+++ philo_bonus/src/state_bonus.c
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 09:10:17 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/02/28 17:47:36 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/03/01 10:07:26 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@ void	eat(t_philo *philo)
 {
 	sem_wait(philo->time->print);
 	sem_wait(philo->time->forks);
-	// printf("%lu	philo %d has taken left fork\n",
-	// 	time_in_ms() - philo->time->start_time, philo->number);
+	printf("%lu	philo %d has taken left fork\n",
+		time_in_ms() - philo->time->start_time, philo->number);
 	sem_post(philo->time->print);
 	sem_wait(philo->time->forks);
 	sem_wait(philo->time->print);
-	// printf("%lu	philo %d has taken right fork\n",
-	// 	time_in_ms() - philo->time->start_time, philo->number);
+	printf("%lu	philo %d has taken right fork\n",
+		time_in_ms() - philo->time->start_time, philo->number);
 	sem_post(philo->time->print);
 	sem_wait(philo->time->print);
-	// printf("%lu	philo %d is eating\n",
-	// 	time_in_ms() - philo->time->start_time, philo->number);
+	printf("%lu	philo %d is eating\n",
+		time_in_ms() - philo->time->start_time, philo->number);
 	sem_post(philo->time->print);
 	philo->last_meal = time_in_ms();
 	philo->state = EAT;
@@ -38,8 +38,8 @@ void	eat(t_philo *philo)
 void	think(t_philo *philo)
 {
 	sem_wait(philo->time->print);
-	// printf("%lu	philo %d	is thinking\n",
-	// 	time_in_ms() - philo->time->start_time, philo->number);
+	printf("%lu	philo %d	is thinking\n",
+		time_in_ms() - philo->time->start_time, philo->number);
 	philo->state = THINK;
 	sem_post(philo->time->print);
 }
@@ -47,8 +47,8 @@ void	think(t_philo *philo)
 void	slp(t_philo *philo)
 {
 	sem_wait(philo->time->print);
-	// printf("%lu	philo %d	is sleeping\n",
-	// 	time_in_ms() - philo->time->start_time, philo->number);
+	printf("%lu	philo %d	is sleeping\n",
+		time_in_ms() - philo->time->start_time, philo->number);
 	philo->state = SLEEP;
 	sem_post(philo->time->print);
 	sleep_ms(philo->time->time_to_eat);
